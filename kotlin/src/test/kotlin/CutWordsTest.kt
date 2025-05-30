c40c630a17fbc41f1725a419558128f1ae995c34
Alejandro-Bravo2 <abravoc@iesrafaelalberti.es>
2025-05-30 18:34:35 +0200

    rename test name

--- kotlin/src/test/kotlin/CutWordsTest.kt
+++ kotlin/src/test/kotlin/CutWordsTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.TestFactory
 class CutWordsTest {
 
     @Test
-    fun change_me_first() {
+    fun split_words_with_single_syllable() {
         val cutWords = CutWords()
         Assertions.assertEquals("sol", cutWords.split("sol"))
     }
