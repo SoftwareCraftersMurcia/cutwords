32875416cced51b065246ed5b4c10bc703ce90bc
Alberto Morales <kodenix@gmail.com>
2025-05-30 19:11:24 +0200

    Refactor CutWords class to improve handling of four-letter words

--- kotlin/src/main/kotlin/CutWords.kt
+++ kotlin/src/main/kotlin/CutWords.kt
@@ -4,13 +4,15 @@ class CutWords {
         val list_vocal = listOf<Char>('a', 'e', 'i', 'o', 'u')
         var result = ""
         for (letre in word) {
+            result += letre
             if (letre in list_vocal) {
-                result += letre + "-"
+                result += "-"
             }
         }
+        result = result.substring(0,result.length-1);
 
         if (word == "chilla") return "chi-lla"
-        if (word.length == 4) return word.substring(0,2)+"-"+word.substring(2,4)
+        if (word.length == 4) return result
         if (word.length == 5) return word.substring(0,2)+"-"+word.substring(2,5)
         else return word
     }
