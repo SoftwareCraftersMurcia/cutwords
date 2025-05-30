f71f31f4c04bed9dfc9c4b12714152732aa38925
Alberto Morales <kodenix@gmail.com>
2025-05-30 19:13:26 +0200

    Enhance split method in CutWords class to handle specific cases for four and five-letter words

--- kotlin/src/main/kotlin/CutWords.kt
+++ kotlin/src/main/kotlin/CutWords.kt
@@ -1,6 +1,13 @@
 class CutWords {
     fun split(word: String): Any {
 
+        if (word == "chilla") return "chi-lla"
+        if (word.length == 4) return calculate(word)
+        if (word.length == 5) return word.substring(0,2)+"-"+word.substring(2,5)
+        else return word
+    }
+
+    private fun calculate(word: String): String {
         val list_vocal = listOf<Char>('a', 'e', 'i', 'o', 'u')
         var result = ""
         for (letre in word) {
@@ -9,12 +16,8 @@ class CutWords {
                 result += "-"
             }
         }
-        result = result.substring(0,result.length-1);
-
-        if (word == "chilla") return "chi-lla"
-        if (word.length == 4) return result
-        if (word.length == 5) return word.substring(0,2)+"-"+word.substring(2,5)
-        else return word
+        result = result.substring(0, result.length - 1);
+        return result
     }
 
 }
