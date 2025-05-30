da7eef580ac648fbc49744b759bdf99ed6e32070
Alejandro-Bravo2 <abravoc@iesrafaelalberti.es>
2025-05-30 19:06:31 +0200

    recorrer palabra

--- kotlin/src/main/kotlin/CutWords.kt
+++ kotlin/src/main/kotlin/CutWords.kt
@@ -1,5 +1,14 @@
 class CutWords {
     fun split(word: String): Any {
+
+        val list_vocal = listOf<Char>('a', 'e', 'i', 'o', 'u')
+        var result = ""
+        for (letre in word) {
+            if (letre in list_vocal) {
+                result += letre + "-"
+            }
+        }
+
         if (word == "chilla") return "chi-lla"
         if (word.length == 4) return word.substring(0,2)+"-"+word.substring(2,4)
         if (word.length == 5) return word.substring(0,2)+"-"+word.substring(2,5)
