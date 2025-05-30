53e90f6190a78ebdac6cdb20ad898b7e990a32b9
Fabrizio Fasanando <fabriziofasanando2@gmail.com>
2025-05-30 18:34:53 +0200

    test(red): tijera

--- php/tests/MyClassTest.php
+++ php/tests/MyClassTest.php
@@ -7,13 +7,21 @@ use PHPUnit\Framework\TestCase;
 
 class MyClassTest extends TestCase
 {
-    /** @test */
-    public function give_me_a_good_name_please(): void
+    public function test_maraca(): void
     {
         $cutter = new CutWords();
 
         $result = $cutter->word("maraca");
 
         self::assertSame("ma-ra-ca", $result);
+    }
+
+    public function test_tijera(): void
+    {
+        $cutter = new CutWords();
+
+        $result = $cutter->word("tijera");
+
+        self::assertSame("ti-je-ra", $result);
     }
 }
